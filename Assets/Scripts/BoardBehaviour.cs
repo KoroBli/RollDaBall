a8c64c695febe1d2bc190a2237f19ab5846ca489
KoroBli <jordijorba@santmarc.com>
2019-02-28 17:04:29 +0100

    It Works!!!!

---
+++ Assets/Scripts/BoardBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardBehaviour : MonoBehaviour {
+
+    float rotationX = 0f;
+    float rotationY = 0f;
+    float speed = 10f;
+    
+	// Use this for initialization
+	void Start ()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        rotationX = Input.GetAxis("Mouse X") * Time.deltaTime * speed;
+        rotationY = Input.GetAxis("Mouse Y") * Time.deltaTime * speed;
+
+        transform.localEulerAngles = new Vector3(transform.localEulerAngles.x - rotationY, 0, transform.localEulerAngles.z + rotationX);
+
+        if (transform.localEulerAngles.x >= 30 && transform.localEulerAngles.x <= 35) transform.localEulerAngles = new Vector3(30, 0, transform.localEulerAngles.z + rotationX);
+        else if (transform.localEulerAngles.x <= 330 && transform.localEulerAngles.x >= 325) transform.localEulerAngles = new Vector3(-30, 0, transform.localEulerAngles.z + rotationX);
+
+        if (transform.localEulerAngles.z >= 30 && transform.localEulerAngles.z <= 35) transform.localEulerAngles = new Vector3(transform.localEulerAngles.x + rotationY, 0, 30);
+        else if (transform.localEulerAngles.z <= 330 && transform.localEulerAngles.z >= 325) transform.localEulerAngles = new Vector3(transform.localEulerAngles.x + rotationY, 0, -30);
+    }
+}
---
+++ Assets/Scripts/UIBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIBehaviour : MonoBehaviour {
+
+    public Transform arrow;
+
+    public Text countdownText;
+
+    private int direction = 1;
+    private int speed = 5;
+
+    private float timeCounter = 0f;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        timeCounter += Time.deltaTime;
+
+        countdownText.text = Mathf.Round(timeCounter).ToString();
+        arrow.Translate(0, speed * Time.deltaTime * direction, 0);
+
+        if (arrow.position.z <= -60) direction *= -1;
+        else if (arrow.position.z >= -55) direction *= -1;
+    }
+}
